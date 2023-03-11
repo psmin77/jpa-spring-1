39a48a91bf573700b372d6d6dcfa31b8de3e77c7
Seongmin <chr92@naver.com>
2023-03-11 15:24:40 +0900

    [Section5-1] Item Entity

--- jpashop/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain.item;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +25,18 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+    //==== 비즈니스 로직 ====//
+    /* 재고 증가 */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /* 재고 감소 */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity -= quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("Need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
